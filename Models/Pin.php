cd2848036ac7c4b0fe2304a84741b680b8d30b68
Michelangelo <sisma99@gmail.com>
2018-12-22 16:49:23 +0100

    Support for PIN added

--- Models/Pin.php
+++ Models/Pin.php
@@ -4,6 +4,7 @@ namespace Modules\Bar\Models;
 
 use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Pin extends Model {
 
@@ -11,6 +12,14 @@ class Pin extends Model {
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public static function generate(){
+        $num_str = sprintf("%06d", mt_rand(1, 999999));
+        return [
+            'encrypted' => Crypt::encrypt($num_str),
+            'pin' => $num_str
+        ];
     }
 
 }
